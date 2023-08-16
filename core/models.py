74c7149adb8bb5efc6b51084b1208d5ae1106859
Thiago H. Hormann <thiago.h.hormann@gmail.com>
2023-08-16 09:12:51 -0300

    Add core app to project. Create models for project and add migrations.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    biography = models.CharField(max_length=300, blank=True)
+    avatar = models.ImageField(upload_to="core/avatars", blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    social_media = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    categories = models.ManyToManyField(Category, related_name="posts")
+
+    def __str__(self):
+        return f"{self.title} - {self.user} - {self.date}"
+
+
+class Comment(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Media(models.Model):
+    file = models.FileField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.file.name
