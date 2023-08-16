77618a6d3cac1f701a48c1e516b1b358c19d9776
Thiago H. Hormann <thiago.h.hormann@gmail.com>
2023-08-16 12:49:24 -0300

    Add custom User model and create views.

--- core/models.py
+++ core/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
-from django.contrib.auth.models import User
+
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    # add your custom fields here
 
 
 class Profile(models.Model):
